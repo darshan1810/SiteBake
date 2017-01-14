da22ac3a3369ec8d590d164275a82c44ecf04b12
Darshan Godhia <darshan1810@gmail.com>
2017-01-13 19:20:32 -0500

    Added links in 'about me', added 'updated on section, and contact

--- data/bio.js
+++ data/bio.js
@@ -2,18 +2,20 @@ var _bio = {
     "name": "Darshan Godhia",
     "photo_link": "./data/photo.jpg",
     "tagline": "\"Everyone knows that debugging is twice as hard as writing a program in the first place. " +
-    "So if you're as clever as you can be when you write it, how will you ever debug it?\" " +
-    " - Brian Kernighan",
-    "aboutme": [
-        {
+        "So if you're as clever as you can be when you write it, how will you ever debug it?\" " +
+        " - Brian Kernighan",
+    "aboutme": [{
             "type": "para",
-            "text": "I am a graduate student at SUNY Stony Brook, NY, pursuing Masters of Science in Computer Science."
+            "text": "I am a graduate student at <a href='http://www.stonybrook.edu/' target='_blank'>SUNY Stony Brook, NY</a>, " +
+                "pursuing Masters of Science in <a href='https://www.cs.stonybrook.edu/' target='_blank'>Computer Science.</a>"
         },
         {
             "type": "para",
-            "text": "I am a part of the File Systems and Storage Lab. I will be involved in research projects in the " +
-            "domain of Storage Systems under the guidance of Prof. Erez Zadok."
+            "text": "I am a part of the <a href='http://www.fsl.cs.sunysb.edu/' target='_blank'>File Systems and Storage Lab</a>. " +
+                "I will be involved in research projects in the " +
+                "domain of Storage Systems under the guidance of " +
+                "<a href='http://www3.cs.stonybrook.edu/~ezk/'>Prof. Erez Zadok.</a>"
         }
     ],
-    "default_email": "dgodhia AT cs DOT stonybrook DOT edu"
+    "default_email": "dgodhia+website@cs.stonybrook.edu"
 };
--- js/controllers.js
+++ js/controllers.js
@@ -1,10 +1,14 @@
 angular.module('myApp.controllers', ['myApp.data'])
-    .controller('biocontroller', ['$scope', 'bio',
-        function ($scope, $bio) {
+    .controller('biocontroller', ['$scope', 'bio', '$sce',
+        function($scope, $bio, $sce) {
+            $scope.renderHtml = function(htmlCode) {
+                return $sce.trustAsHtml(htmlCode);
+            };
             $scope.bio = $bio;
-    }])
+        }
+    ])
     .controller('navbarcontroller', ['$scope', 'bio',
-        function ($scope, $bio) {
+        function($scope, $bio) {
             $scope.name = $bio.name;
             if ($bio.resumelink || $bio.default_email) {
                 $scope.showRightNav = true;
@@ -15,28 +19,35 @@ angular.module('myApp.controllers', ['myApp.data'])
             $scope.default_email = "mailto:" + $bio.default_email;
             if ($bio.default_email) $scope.hasDefaultEmail = true;
             else $scope.hasDefaultEmail = false;
-    }])
+        }
+    ])
     .controller('educationcontroller', ['$scope', 'education',
-        function ($scope, $education) {
+        function($scope, $education) {
             $scope.education = $education;
-    }])
+        }
+    ])
     .controller('extracontroller', ['$scope', 'extra',
-        function ($scope, $extra) {
+        function($scope, $extra) {
             $scope.extra = $extra;
-    }])
+        }
+    ])
     .controller('workexperiencecontroller', ['$scope', 'workexperience',
-        function ($scope, $workexperience) {
+        function($scope, $workexperience) {
             $scope.workexperience = $workexperience;
-    }])
+        }
+    ])
     .controller('projectscontroller', ['$scope', 'projects',
-        function ($scope, $projects) {
+        function($scope, $projects) {
             $scope.projects = $projects;
-    }])
+        }
+    ])
     .controller('webpresencecontroller', ['$scope', 'webpresence',
-        function ($scope, $webpresence) {
+        function($scope, $webpresence) {
             $scope.webpresence = $webpresence;
-    }])
+        }
+    ])
     .controller('skillscontroller', ['$scope', 'skills',
-        function ($scope, $skills) {
+        function($scope, $skills) {
             $scope.skills = $skills;
-    }]);
+        }
+    ]);
